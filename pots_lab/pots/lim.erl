6511c16f19bc9a71c2640b08c71f50f8d279bbe8
uwiger <uwiger@e12cd91d-2f5b-425b-b2d1-ad981c3e8913>
2010-01-21 18:23:08 +0000

    added license texts
    git-svn-id: http://svn.ulf.wiger.net/pots/trunk@2 e12cd91d-2f5b-425b-b2d1-ad981c3e8913

--- pots_lab/pots/lim.erl
+++ pots_lab/pots/lim.erl
@@ -1,3 +1,21 @@
+%%
+%% %CopyrightBegin%
+%% 
+%% Copyright Ericsson AB 1996-2009. All Rights Reserved.
+%% 
+%% The contents of this file are subject to the Erlang Public License,
+%% Version 1.1, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Public License along with this software. If not, it can be
+%% retrieved online at http://www.erlang.org/.
+%% 
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%% 
+%% %CopyrightEnd%
+%%
 -module(lim).
 -export([start/0, start/1, start_tone/1, stop_tone/0,
 	 start_ringing/0, stop_ringing/0, connect_to/1, disconnect_from/1,
